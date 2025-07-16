b24b939734d0e85e67f8e38367927ef2c3ade85e
rahuljoshi3107 <76519389+rahuljoshi3107@users.noreply.github.com>
2025-07-17 00:30:09 +0530

    Add files via upload

---
+++ accountCustomReport.js
@@ -0,0 +1,50 @@
+import { LightningElement, wire } from 'lwc';
+import getAccountList from '@salesforce/apex/AccountController.getAccountList';
+import { loadStyle } from 'lightning/platformResourceLoader';
+// import styles from '@salesforce/resourceUrl/cusstyle';
+
+import styles from '@salesforce/resourceUrl/cssdatatable';
+
+export default class AccountCustomReport extends LightningElement {
+    accounts;
+    error;
+
+    renderedCallback() {
+    Promise.all([
+        loadStyle(this, styles)
+    ]).then(() => {
+        window.console.log('Files loaded.');
+    }).catch(error => {
+        window.console.log("Error " + error.body.message);
+    });
+}
+
+    columns = [
+        { label: 'Name', fieldName: 'Name' },
+        { label: 'Industry', fieldName: 'Industry' },
+        { label: 'Phone', fieldName: 'Phone', type: 'phone' },
+        { label: 'Type', fieldName: 'Type' },
+        { label: 'Website', fieldName: 'Website', type: 'url' },
+        { label: 'Rating', fieldName: 'Rating' },
+        { label: 'Account Number', fieldName: 'AccountNumber' },
+        { label: 'Annual Revenue', fieldName: 'AnnualRevenue', type: 'currency' },
+        { label: 'Billing City', fieldName: 'BillingCity' },
+        { label: 'Billing State', fieldName: 'BillingState' },
+        { label: 'Billing Country', fieldName: 'BillingCountry' }
+    ];
+
+    columns2 = [
+         { label: 'New Name', fieldName: 'Name' }
+    ]
+
+    @wire(getAccountList)
+    wiredAccounts({ data, error }) {
+        if (data) {
+            this.accounts = data;
+            this.error = undefined;
+        } else if (error) {
+            this.error = error;
+            this.accounts = undefined;
+        }
+    }
+}
