36595b6a99083324638654fc7619c631de9b7ba6
rahuljoshi3107 <76519389+rahuljoshi3107@users.noreply.github.com>
2025-12-15 11:56:52 +0530

    Update accountCustomReport.js
    with and without initialWidth

--- accountCustomReport.js
+++ accountCustomReport.js
@@ -20,6 +20,8 @@ export default class AccountCustomReport  extends LightningElement {
         });
     }
 
+   //Without initialWidth
+    /*
     columns = [
         { label: 'Name', fieldName: 'Name' },
         { label: 'Industry', fieldName: 'Industry' },
@@ -32,6 +34,43 @@ export default class AccountCustomReport  extends LightningElement {
         { label: 'Billing City', fieldName: 'BillingCity' },
         { label: 'Billing State', fieldName: 'BillingState' },
         { label: 'Billing Country', fieldName: 'BillingCountry' }
+    ];*/
+
+    //with initialWidth 
+
+    columns = [
+        { label: 'Name', fieldName: 'Name', initialWidth: 200 },
+        { label: 'Industry', fieldName: 'Industry', initialWidth: 150 },
+        { label: 'Phone', fieldName: 'Phone', type: 'phone', initialWidth: 150 },
+        { label: 'Type', fieldName: 'Type', initialWidth: 140 },
+        { label: 'Website', fieldName: 'Website', type: 'url', initialWidth: 220 },
+        { label: 'Rating', fieldName: 'Rating', initialWidth: 120 },
+        { label: 'Account Number', fieldName: 'AccountNumber', initialWidth: 160 },
+        {
+            label: 'Annual Revenue',
+            fieldName: 'AnnualRevenue',
+            type: 'currency',
+            initialWidth: 160
+        },
+        { label: 'Billing City', fieldName: 'BillingCity', initialWidth: 150 },
+        { label: 'Billing State', fieldName: 'BillingState', initialWidth: 140 },
+        { label: 'Billing Country', fieldName: 'BillingCountry', initialWidth: 160 },
+        { label: 'Name', fieldName: 'Name', initialWidth: 200 },
+        { label: 'Industry', fieldName: 'Industry', initialWidth: 150 },
+        { label: 'Phone', fieldName: 'Phone', type: 'phone', initialWidth: 150 },
+        { label: 'Type', fieldName: 'Type', initialWidth: 140 },
+        { label: 'Website', fieldName: 'Website', type: 'url', initialWidth: 220 },
+        { label: 'Rating', fieldName: 'Rating', initialWidth: 120 },
+        { label: 'Account Number', fieldName: 'AccountNumber', initialWidth: 160 },
+        {
+            label: 'Annual Revenue',
+            fieldName: 'AnnualRevenue',
+            type: 'currency',
+            initialWidth: 160
+        },
+        { label: 'Billing City', fieldName: 'BillingCity', initialWidth: 150 },
+        { label: 'Billing State', fieldName: 'BillingState', initialWidth: 140 },
+        { label: 'Billing Country', fieldName: 'BillingCountry', initialWidth: 160 }
     ];
 
     
